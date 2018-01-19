b535d401a180ba2623b16510a84a3e32d66b6666
Javier <javiergarciaramos@gmail.com>
2018-01-19 16:46:52 +0000

    Create a Gig (WIP)

---
+++ GigHub/GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public String Date { get; set; }
+
+        public string Time { get; set; }
+    }
+}
