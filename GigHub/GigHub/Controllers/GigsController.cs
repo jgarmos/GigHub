8e63b8541609475bd916edabbb7c7b80b262edbc
Javier Garcia <javiergarciaramos@gmail.com>
2018-01-22 06:54:35 +0000

    Add a Gig Post (WIP)

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -2,6 +2,8 @@
 using GigHub.ViewModels;
 using System.Linq;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System;
 
 namespace GigHub.Controllers
 {
@@ -15,6 +17,7 @@ namespace GigHub.Controllers
             _context = new ApplicationDbContext();
         }
 
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -23,6 +26,26 @@ namespace GigHub.Controllers
             };
             return View(viewModel);
 
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create (GigFormViewModel viewModel)
+        {
+            var artistId = User.Identity.GetUserId();
+            var artist = _context.Users.Single(u => u.Id == artistId);
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+            var gig = new Gig()
+            {
+                Artist = artist,
+                DateTime = DateTime.Parse(string.Format("{0} {1}",viewModel.Date,viewModel.Time)),
+                Genre = genre,
+                Venue = viewModel.Venue
+            };
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
         }
     }
 }
