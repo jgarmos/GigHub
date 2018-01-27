f0aa71473f52321f85a95dd7a6a4033a75ea4acb
Javier Garcia <javiergarciaramos@gmail.com>
2018-01-27 23:21:19 +0000

    Implement validations. Server validation. Custom validation and enable client jquery validation

--- GigHub/GigHub/Controllers/GigsController.cs
+++ GigHub/GigHub/Controllers/GigsController.cs
@@ -19,7 +19,7 @@ namespace GigHub.Controllers
 
         [Authorize]
         public ActionResult Create()
-        {
+        {   
             var viewModel = new GigFormViewModel
             {
                 Genres = _context.Genres.ToList()
@@ -32,13 +32,20 @@ namespace GigHub.Controllers
         [HttpPost]
         public ActionResult Create (GigFormViewModel viewModel)
         {
+
+            if (!ModelState.IsValid)
+            {
+                viewModel.Genres = _context.Genres.ToList();
+                return View("Create", viewModel);
+            }
+
             var artistId = User.Identity.GetUserId();
             var artist = _context.Users.Single(u => u.Id == artistId);
             var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
             var gig = new Gig()
             {
                 Artist = artist,
-                DateTime = viewModel.DateTime,
+                DateTime = viewModel.GetDateTime(),
                 Genre = genre,
                 Venue = viewModel.Venue
             };
