97b4712950062a4a77ae6833bcbd69071b348031
Javier <javiergarciaramos@gmail.com>
2018-02-08 16:48:31 +0000

    New style index page with list of upcomings gigs. Including genre.

--- GigHub/GigHub/Controllers/HomeController.cs
+++ GigHub/GigHub/Controllers/HomeController.cs
@@ -19,7 +19,9 @@ namespace GigHub.Controllers
         public ActionResult Index()
         {
             //var upcomingGigs = _context.Gigs.Include(g => g.Artist).Where(g => g.DateTime > DateTime.Now);
-            var upcomingGigs = _context.Gigs.Include(g => g.Artist);
+            var upcomingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g => g.Genre);
             return View(upcomingGigs);
         }
 
